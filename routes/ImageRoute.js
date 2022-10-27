6addacf4cf710d4db98ac208eac0c3e9c1f73d43
Khoa Dang <khoa9205141@gmail.com>
2022-10-28 02:38:07 +0700

    temp

--- routes/ImageRoute.js
+++ routes/ImageRoute.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const imageController = require("../controllers/ImageController");
 
-router.get("/upload", imageController.Upload);
+//router.get("/upload", imageController.Upload);
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const userRoute = require("./UserRoute");
 const imageRoute = require("./ImageRoute");
 function route(app) {
   app.use("/api/auth", userRoute);
-  app.use("/api/image", imageRoute);
+  //app.use("/api/image", imageRoute);
   app.get("/api", (req, res) => res.send("hello"));
 }
 
