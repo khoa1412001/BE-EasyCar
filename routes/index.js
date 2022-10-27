239fa94d78781e672dd37cda7088e839dfa2bff4
Khoa Dang <khoa9205141@gmail.com>
2022-10-27 14:32:25 +0700

    image controllr

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const userRoute = require("./UserRoute");
-
+const imageRoute = require("./ImageRoute");
 function route(app) {
   app.use("/api/auth", userRoute);
-
+  app.use("/api/image", imageRoute);
   app.get("/api", (req, res) => res.send("hello"));
 }
 
