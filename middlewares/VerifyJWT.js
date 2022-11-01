b9c41b62197c45c0b47805b33c4183a7cbcb02ad
Khoa Dang <khoa9205141@gmail.com>
2022-11-01 20:40:27 +0700

    pls

---
+++ middlewares/VerifyJWT.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config;
+function authenticateToken(req, res, next) {
+  const authHeader =
+    req.headers["authorization"] || req.headers["Authorization"];
+  if (!authHeader?.startsWith("Bearer")) return res.sendStatus(401);
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_KEY, (err, decoded) => {
+    if (err) {
+      console.log(err.message);
+      return res.status(403).send("Unauthenticated");
+    }
+    req.user = {
+      userId: decoded.userId,
+      role: decoded.role,
+    };
+    next();
+  });
+}
+
+module.exports = authenticateToken;
