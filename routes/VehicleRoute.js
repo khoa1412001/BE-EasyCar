2599f37cdeb6cb71c3f04415a3a917ec812a5082
Khoa Dang <khoa9205141@gmail.com>
2022-11-01 19:33:38 +0700

    Update VehicleRoute.js

--- routes/VehicleRoute.js
+++ routes/VehicleRoute.js
@@ -2,10 +2,10 @@ const router = require("express").Router;
 const vehicleController = require("../controllers/VehicleController");
 const uploadImage = require("../middlewares/UploadImage");
 const passport = require("../middlewares/VerifyJWT");
-router.post(
-  "/add-vehicle",
-  passport,
-  uploadImage.array("vehicle"),
-  vehicleController.AddVehicle
-);
+// router.post(
+//   "/add-vehicle",
+//   passport,
+//   uploadImage.array("vehicle"),
+//   vehicleController.AddVehicle
+// );
 module.exports = router;
