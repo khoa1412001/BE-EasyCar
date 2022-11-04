be53341eac443b0b236120f853dfcd2b4320398e
Khoa Dang <khoa9205141@gmail.com>
2022-11-04 22:37:06 +0700

    1

---
+++ utils/Cloudinary.js
@@ -0,0 +1,30 @@
+require("dotenv").config;
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+cloudinary.config({
+  secure: true,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+function uploadFile(file) {
+  return new Promise((resolve, reject) => {
+    let stream = cloudinary.uploader.upload_stream(
+      {
+        folder: file.fieldname,
+        resource_type: "auto",
+      },
+      (error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+
+    streamifier.createReadStream(file.buffer).pipe(stream);
+  });
+}
+module.exports = uploadFile;
