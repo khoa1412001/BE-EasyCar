04da0aa30c0137243b0ed321f171f03c10ca9a09
Khoa Dang <85307735+khoa1412001@users.noreply.github.com>
2022-11-08 17:30:31 +0700

    Delete verifyRoles.js

--- middlewares/verifyRoles.js
+++
@@ -1,9 +0,0 @@
-function verifyRoles(...allowedRoles) {
-  return (req, res, next) => {
-    const userRole = req.user.role;
-    if (!allowedRoles.includes(userRole))
-      return res.status(401).send("Unauthorized");
-    next();
-  };
-}
-module.exports = verifyRoles;
