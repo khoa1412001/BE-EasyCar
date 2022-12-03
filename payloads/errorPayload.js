629feec31554d428194d5a8826f5de1e00993f54
Khoa Dang <khoa9205141@gmail.com>
2022-12-03 20:50:02 +0700

    Delete errorPayload.js

--- payloads/errorPayload.js
+++
@@ -1,5 +0,0 @@
-function errorPayload(res, error) {
-  console.log(error.message);
-  return res.status(400).json({ message: "Lỗi hệ thống vui lòng thử lại sau" });
-}
-module.exports = errorPayload;
