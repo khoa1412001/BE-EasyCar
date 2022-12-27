066138cdc328e50e227e46a6bb09123c606fccc9
Khoa Dang <khoa9205141@gmail.com>
2022-12-28 01:25:47 +0700

    Update Sendmail.js

--- services/Sendmail.js
+++ services/Sendmail.js
@@ -3,25 +3,48 @@ require("dotenv").config;
 async function Sendmail(email, subject, text) {
   var transporter = nodemailer.createTransport({
     // config mail server
+    port: 465,
     service: "gmail",
     auth: {
       user: process.env.MAIL_USERNAME,
       pass: process.env.MAIL_PASSWORD,
     },
+    secure: true
   });
+  
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+});
+
   var mailOptions = {
     from: process.env.MAIL_USERNAME,
     to: email,
     subject: subject,
     text: text,
   };
-  await transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.log(err);
-      return false;
-    }
-    return true;
-  });
+
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            resolve(info);
+        }
+    });
+});
+
+ return true
 }
 
 module.exports = Sendmail;
